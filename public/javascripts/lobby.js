3b5f22d4453051597cb01fef12675c4456e21dee
Muntashir <kazimuntashir@gmail.com>
2015-09-04 18:10:39 -0400

    Merge create and join channel

--- public/javascripts/lobby.js
+++ public/javascripts/lobby.js
@@ -1,5 +1,4 @@
 (function () {
-    var action;
     var roomID;
     var socket;
 
@@ -31,21 +30,9 @@
             }
         });
 
-        $('#create-room-form').submit(function () {
-            action = 'create';
-            if (checkRoomID($('#create-room-text').val())) {
-                roomID = $('#create-room-text').val();
-                socket.emit('check-room', roomID);
-            } else {
-                bootbox.alert("The channel name can only have numbers and letters");
-            }
-            return false;
-        });
-
-        $('#join-room-form').submit(function () {
-            action = 'join';
-            if (checkRoomID($('#join-room-text').val())) {
-                roomID = $('#join-room-text').val();
+        $('#enter-room-form').submit(function () {
+            if (checkRoomID($('#enter-room-text').val())) {
+                roomID = $('#enter-room-text').val();
                 socket.emit('check-room', roomID);
             } else {
                 bootbox.alert("The channel name can only have numbers and letters");
@@ -54,21 +41,8 @@
         });
 
         socket.on('check-room-response', function (response) {
-            if (action === 'create') {
-                if (response) {
-                    bootbox.alert("This channel already exists", function () {});
-                } else {
-                    socket.emit('create-room', roomID);
-                    window.location = "/channels/" + roomID;
-                }
-            }
-            if (action === 'join') {
-                if (response) {
-                    window.location = "/channels/" + roomID;
-                } else {
-                    bootbox.alert("This channel does not exist", function () {});
-                }
-            }
+            if (!response) socket.emit('create-room', roomID);
+            window.location = "/channels/" + roomID;
         });
 
         $(window).on('beforeunload', function () {
